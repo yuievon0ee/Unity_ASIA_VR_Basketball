354893ce0b4e7c618b37b00f18a25b0be0ff96f1
yuievon0ee <71679885+yuievon0ee@users.noreply.github.com>
2020-12-22 12:00:56 +0800

    籃網和VR

---
+++ Unity_VR_Basketball/Assets/ScoreManager.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    [Header("分數介面")]
+    public Text textScore;
+    [Header("分數")]
+    public int Score;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print(other.name);
+    }
+}
